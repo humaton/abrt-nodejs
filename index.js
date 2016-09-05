559795b839295682ae2031c5d47c6dc875520101
Tomas Hrcka <thrcka@redhat.com>
2016-09-05 13:37:33 +0200

    Change executable from node to actual script that is executed by node

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const net = require('net');
-var Socket = require('net').Socket;
+var Socket = net.Socket;
 var client = new Socket();
 // TODO: Get VAR_RUN from ABRT configuration.
 var ABRT_SOCKET_PATH =  '/var/run/abrt/abrt.socket'
@@ -9,7 +9,7 @@ process.on('uncaughtException', (err) => {
     client.connect({path: ABRT_SOCKET_PATH}, function(){
       client.write("PUT / HTTP/1.1\r\n\r\n");
       client.write("PID=" + process.pid + "\0");
-      client.write("EXECUTABLE=" + process.execPath + "\0");
+      client.write("EXECUTABLE=" + process.argv[1] + "\0");
       client.write("ANALYZER=Nodejs\0");
       client.write("TYPE=Javascript\0");
       client.write("BASENAME=jshook\0");
