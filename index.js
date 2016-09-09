88e7fc8e72a19ef2a532f2941f229a73154408a5
Jakub Filak <jfilak@redhat.com>
2016-09-09 14:52:10 +0200

    Print error description to console too
    Using ABRT error detectors should not break regular user workflows.
    Unless something terribly broken, processes should behave like without
    ABRT enabled.
    
    The printed message will differ from the one printed out by Node.js
    because Node.js includes source line where the error occurred but there
    is no such information in Error message included. I hope we will find be
    able to print out exactly the same message in future.
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var client = new Socket();
 // TODO: Get VAR_RUN from ABRT configuration.
 var ABRT_SOCKET_PATH =  '/var/run/abrt/abrt.socket'
 
-process.on('uncaughtException', (err) => {
-    try {
+function send_to_abrt(err) {
+  try {
     client.connect({path: ABRT_SOCKET_PATH}, function(){
       client.write("PUT / HTTP/1.1\r\n\r\n");
       client.write("PID=" + process.pid + "\0");
@@ -17,20 +17,26 @@ process.on('uncaughtException', (err) => {
       client.write("BACKTRACE=" + err.message + "\n\n" + err.stack + "\0");
       client.end();
     });
+  }
+  catch(err) {
+    console.log("Error in connection to abrt socket is abrt running?")
+  }
+
+  client.on('data', (data) => {
+    response_parts = data.toString().split(" ");
+    response_code = parseInt(response_parts[1])
+    if (response_code >= 400) {
+      console.log("Error when sending data to abrt server: " + data.toString())
     }
-    catch(err) {
-      console.log("Error in connection to abrt socket is abrt running?")
-    }
-  
-    client.on('data', (data) => {
-      response_parts = data.toString().split(" ");
-      response_code = parseInt(response_parts[1])
-      if (response_code >= 400) {
-        console.log("Error when sending data to abrt server: " + data.toString())
-      }      
-      client.end();
-    });
+    client.end();
+  });
+}
+
+function print_to_console(err) {
+  console.log(err);
+}
+
+process.on('uncaughtException', (err) => {
+  print_to_console(err);
+  send_to_abrt(err);
 });
-
-
-  
